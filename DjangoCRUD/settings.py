35a9ff386dd33203d2a8d2021e28ec2f96b57343
Santosh Rai <rai.santosh55@gmail.com>
2022-09-14 14:20:41 +0545

    creates and registers the Blog model

--- DjangoCRUD/settings.py
+++ DjangoCRUD/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
+    'crud_app',
 ]
 
 MIDDLEWARE = [
--- crud_app/models.py
+++ crud_app/models.py
@@ -1,3 +1,20 @@
+from distutils.command.upload import upload
+from tkinter import CASCADE
 from django.db import models
+from ckeditor.fields import RichTextField
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE)
+    content = RichTextField(blank=True, null=True)
+    image = models.ImageField(upload_to='crud_app/images/')
+    published_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title + ' by '+str(self.author)
